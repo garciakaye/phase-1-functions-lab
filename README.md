f5bdcf9abd57176d65fc616ff54ae9da7a110481
kaye garcia <kayemgarcia@gmail.com>
2021-08-31 13:59:35 -0500

    commit tree f9674b0e36f0963ae2a82c23e301f26b44593ab6

--- README.md
+++ README.md
@@ -81,8 +81,9 @@ function distanceFromHqInFeet(someValue) {
   long.
 
 - `calculatesFarePrice`: Given the same starting and ending block as the
-  previous test (_hint hint_), return the fare for the customer. The first four
-  hundred feet are free. For a distance between 400 and 2000 feet, the price is 2
-  cents per foot (not including 400, which are free!). Then Scuber charges a
-  flat fare for a distance over 2000 feet and under 2500 feet. Finally, Scuber
+  previous test (_hint hint_), return the fare for the customer.
+  1. The first four hundred feet are free. 
+  2. For a distance between 400 and 2000 feet, the price is 2
+  cents per foot (not including 400, which are free!). 
+  3. Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. Finally, Scuber
   does not allow any rides over 2500 feet — the function returns `'cannot travel that far'` if a ride over 2500 feet is requested.
--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-// Code your solution in this file!
+const BLOCK_DISTANCE_IN_FEET = 264;
+
+function diffInBlocks(start, end = 0){
+    return Math.abs(start - end)
+}
+
+function distanceFromHqInBlocks(blockNumber){
+   const headquartersLocation = 42;
+   return diffInBlocks(headquartersLocation, blockNumber)
+}
+
+function distanceFromHqInFeet(numberOfBlocks){
+    const distanceInBlocks = distanceFromHqInBlocks(numberOfBlocks);
+    return distanceInBlocks * BLOCK_DISTANCE_IN_FEET;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock){
+    const distanceDifferenceInBlocks = diffInBlocks(startingBlock, endingBlock); 
+    return distanceDifferenceInBlocks * BLOCK_DISTANCE_IN_FEET;
+
+}
+
+function calculatesFarePrice(start, end){
+    const distanceTravelled = distanceTravelledInFeet(start, end);
+    if(distanceTravelled <= 400){
+        return 0
+    } else if(distanceTravelled < 2000){
+        return Math.abs(distanceTravelled - 400) * 0.02
+    } else if(distanceTravelled > 2000 && distanceTravelled < 2500){
+        return 25
+    }else {
+        return 'cannot travel that far'
+    }
+
+  
+  
+  
+  //1. calculate distance travelled in feet
+//2. After we calculate the distance travelled in feet, if the distance is less than 400 feet, 
+  // we don't charge a fee.
+
+    //3. But if the distance travelled is between 400 and 2000 feet, the price is 2 cents per foot
+
+  //4. For a distance over 2000 feet and under 2500 feet, we charge a flat rate
+
+  //5. If distance travelled is over 2500 feet, we can't do it
+
+
+}
+
+// function proRateAmount(distance, freeAmount, costPerFoot){
+//     return Math.abs(distance - freeAmount) * costPerFoot
+// }
+
+// function calculatesFarePrice(start, end){
+//     //1. calculate distance travelled in feet
+//       const distanceTravelled = distanceTravelledInFeet(start, end);
+
+//     const isFree = distanceTravelled <= 400;
+//     const isProrated = distanceTravelled < 2000
+//     const isFlatRate = !isProrated && distanceTravelled < 2500
+//     const isTooFar = distanceTravelled > 2500
+    
+
+//     if(isTooFar){
+//         return 'cannot travel that far'
+//     }
+
+//     if(isFlatRate){
+//         return 25
+//     }
+
+//     if(isProrated){
+//         return proRateAmount(distanceTravelled, 400, 0.02)
+//     }
+
+//     return 0
+//   }
